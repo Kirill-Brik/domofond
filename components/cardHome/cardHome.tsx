82a29d6af07dc331c61cecfab646b6debfd20f88
Kirill Brik <kirill.brik@yandex.ru>
2022-06-23 10:17:31 +0300

    feat: доделан компонент карточки дома

--- components/cardHome/cardHome.tsx
+++ components/cardHome/cardHome.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import Card from "../card/card";
+import ButtonIcon from "../buttonIcon/buttonIcon";
 
 import { IHome } from "../../utils/interfaces";
 
@@ -9,13 +10,16 @@ import styles from "./cardHome.module.scss";
 export default function CardHome({ address, contract, reports }: IHome) {
   return (
     <Card className={styles.container}>
-      <h3>{address}</h3>
-      <button>{contract}</button>
-      <ul>
+      <h3 className={styles.address}>{address}</h3>
+      <div className={styles.contract}>
+        <ButtonIcon type="link" url={contract} icon="assignment" />
+        <span className={styles.contractText}>Договор управления</span>
+      </div>
+      <ul className={styles.reportList}>
         {reports.map((report, index) => (
           <li key={index}>
             <Link href={report} passHref>
-              <a>Отчет</a>
+              <a className={styles.reportLink}>Отчет</a>
             </Link>
           </li>
         ))}
