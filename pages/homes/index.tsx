58be515af966dc296052f8818f2adbe2edce1cac
Kirill Brik <kirill.brik@yandex.ru>
2022-07-12 12:57:26 +0300

    feat: доделал страницу с домами

--- pages/homes/index.tsx
+++ pages/homes/index.tsx
@@ -1,14 +1,37 @@
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import Head from "next/head";
-import type { NextPage } from "next";
+import CardHome from "../../components/cardHome/cardHome";
+import { IHome } from "../../utils/interfaces";
+import { getHomeData } from "../../utils/utils";
 
-const Home: NextPage = () => {
+import styles from "./index.module.scss";
+
+export const getStaticProps: GetStaticProps = async () => {
+  let homeList = await getHomeData();
+  return {
+    props: { homeList },
+  };
+};
+
+const Index: NextPage = ({
+  homeList,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <Head>
         <title>Наши дома</title>
       </Head>
+      <section className={styles.container}>
+        <ul className={styles.homeList}>
+          {homeList.map((homeData: IHome, index: number) => (
+            <li key={index}>
+              <CardHome home={homeData}></CardHome>
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 };
 
-export default Home;
+export default Index;
